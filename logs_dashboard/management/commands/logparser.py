fb0018707ab7edd54fa46fc6dedbad0de18743ef
RussBallard <41388123+RussBallard@users.noreply.github.com>
2020-07-02 00:07:12 +0400

    some improvements of custom commands

--- logs_dashboard/management/commands/logparser.py
+++ logs_dashboard/management/commands/logparser.py
@@ -1,7 +1,5 @@
 import os
-import pathlib
 import re
-from urllib.parse import urlparse
 
 import requests
 from django.core.management.base import BaseCommand, CommandError
@@ -12,52 +10,49 @@ from logs_dashboard.models import LogEntry
 
 class Command(BaseCommand):
     help = 'the custom command allows to download the log file and insert data into the database'
-    LOG_REGEX = re.compile(r'(?P<ip>[(\d\.)]+) - - \[(?P<date>.*?)\] "(?P<method>.*?) (?P<request_path>.*?) '
-                           r'HTTP\/(?P<http_version>.*?)" (?P<status_code>\d+) (?P<response_size>\d+|-) '
-                           r'"(?P<referrer>.*?)" "(?P<user_agent>.*?)"')
+    LOG_REGEX = re.compile(r'(?P<ip>.*) - - \[(?P<date>.*)\] "(?P<method>\w*) (?P<request_path>.*) '
+                           r'HTTP\/(?P<http_version>.*)" (?P<status_code>\d+) (?P<response_size>\d+) '
+                           r'"(?P<referrer>.*)" "(?P<user_agent>.*)" "-"')
 
     def add_arguments(self, parser):
         parser.add_argument('log_url', type=str, action='store', help='url to the log file')
-        parser.add_argument('-withoutdelete', action='store_true', help='file will not be deleted if passed')
 
     def handle(self, **options):
-        logs_folder_path = f'{pathlib.Path(__file__).parent.absolute()}/logs_folder/'
-        if not os.path.exists(logs_folder_path):
-            os.makedirs(logs_folder_path)
-
         with requests.get(options['log_url'], stream=True) as r:
             if not r.ok:
                 raise CommandError(f'URL status code: {r.status_code}')
 
-            file_name = f"{urlparse(options['log_url']).netloc}.log"
+            with tqdm(total=int(r.headers['Content-Length'])) as pbar:
+                not_ended_line = None  # Переменная необходима, чтобы перенести незаконченную строку на следующий цикл
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        for line in chunk.decode('utf-8').splitlines():
+                            try:
+                                if not_ended_line:
+                                    line = not_ended_line + line
 
-            with open(file_name, 'wb') as file:
-                with tqdm(total=int(r.headers['Content-Length'])) as pbar:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        if chunk:
-                            file.write(chunk)
-                            pbar.update(len(chunk))
+                                data = self.LOG_REGEX.match(line).groupdict()
 
-                file.seek(0)
+                                LogEntry(ip=data['ip'],
+                                         date=data['date'],
+                                         method=data['method'],
+                                         request_path=data['request_path'],
+                                         http_version=data['http_version'],
+                                         status_code=data['status_code'],
+                                         response_size=data['response_size'] if data['response_size'].isdigit() else 0,
+                                         referrer=data['referrer'],
+                                         user_agent=data['user_agent']
+                                         ).save()
 
-                for line in file:
-                    data = self.LOG_REGEX.match(line.decode('utf-8'))
-                    try:
-                        data = data.groupdict()
+                                not_ended_line = None  # Должна быть всегда None, если регулярка успешно совпала
 
-                        LogEntry(
-                            ip=data['ip'],
-                            date=data['date'],
-                            method=data['method'],
-                            request_path=data['request_path'],
-                            http_version=data['http_version'],
-                            status_code=data['status_code'],
-                            response_size=data['response_size'] if data['response_size'] != '-' else 0,
-                            referrer=data['referrer'],
-                            user_agent=data['user_agent']
-                        ).save()
+                            except AttributeError:  # В случае несовпадения регулярки, строку переносим на другой цикл
+                                if line:
+                                    not_ended_line = line
+                            # except Exception as err:
+                            #     print_err = f'ERR: {err}|nLINE: {line}'
+                            #     print(print_err)
 
-                    except AttributeError:
-                        pass
+                        pbar.update(len(chunk))
 
             self.stdout.write(self.style.SUCCES('Custom command logparser completed'))
--- logs_dashboard/models.py
+++ logs_dashboard/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class LogEntry(models.Model):
-    ip = models.CharField(max_length=300)
-    date = models.CharField(max_length=300)
-    method = models.CharField(max_length=300)
+    id = models.AutoField(primary_key=True)
+    ip = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    method = models.CharField(max_length=10)
     request_path = models.CharField(max_length=1000)
-    http_version = models.CharField(max_length=300)
-    status_code = models.IntegerField()
-    response_size = models.IntegerField()
+    http_version = models.CharField(max_length=5)
+    status_code = models.CharField(max_length=3)
+    response_size = models.PositiveIntegerField(default=0)
     referrer = models.CharField(max_length=1000)
     user_agent = models.CharField(max_length=300)
