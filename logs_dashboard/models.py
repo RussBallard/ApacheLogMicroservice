b103fb6c9331abbbbae2750d73204257848af167
RussBallard <41388123+RussBallard@users.noreply.github.com>
2020-07-20 13:23:28 +0400

    improved log parser script and added some css to html

--- logs_dashboard/models.py
+++ logs_dashboard/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 
+# Если встречается неоправдано большая вместительность поля, то это для строчек с попыткой взлома
 class LogEntry(models.Model):
     id = models.AutoField(primary_key=True)
     ip = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
     date = models.CharField(max_length=50)
-    method = models.CharField(max_length=10)
+    method = models.CharField(max_length=1500)
     request_path = models.CharField(max_length=1000)
     http_version = models.CharField(max_length=5)
     status_code = models.CharField(max_length=3)
